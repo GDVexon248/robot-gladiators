b61b2ef8ad2ea07fe8473ca84506fb4eff04e770
GD <gdvexon248@gmail.com>
2022-01-18 13:46:36 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,13 +22,14 @@ var fight = function(enemyName) {
 
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
             // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -42,7 +43,8 @@ var fight = function(enemyName) {
         }
 
            // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
            // Log a resulting message to the console so we know that it worked.
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -65,7 +67,7 @@ var startGame = function(){
       if (playerHealth > 0) {
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
         var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
         fight(pickedEnemyName);
 
         if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -139,6 +141,11 @@ var shop = function() {
             shop();
             break;
     }
+};
+
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    return value;
 };
 
 startGame();
